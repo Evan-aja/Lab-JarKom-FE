7ab63d4381be960124fc6c4f33363c62b6896e24
notgood54 <138552125+tidakbagus@users.noreply.github.com>
2023-08-05 17:57:22 +0700

    Profil-Narahubung

---
+++ components/profile-card-container.tsx
@@ -0,0 +1,23 @@
+import Card from "./profile-card";
+import articles from "../pages/dummyData";
+
+const CardContainer: React.FC = () => {
+    return (
+        <div className="container mx-auto px-4">
+            <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-7">
+                {articles.map((article) => (
+                    <Card
+                        key={article.id}
+                        id={article.id}
+                        title={article.title}
+                        image={article.image}
+                        content={article.content}
+                        link={article.link}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CardContainer;
